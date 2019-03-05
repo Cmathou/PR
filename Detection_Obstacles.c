ff61d0d8b637060eaa1672b79e8f2677bbb17194
wiki05 <47415177+wiki05@users.noreply.github.com>
2019-03-05 11:24:15 +0100

    Add files via upload

--- Detection_Obstacles.c
+++ Detection_Obstacles.c
@@ -10,13 +10,16 @@
 //
 //------------------------------------------------------------------------------------
 #include "c8051F020.h"
-#include "definitions.h"
+//#include "definitions.h"
 
 //sbit LED = P1 ^ 6;
-sbit MD = P3 ^ 3;
+sbit MD = P3 ^ 2;
+sbit MD_A = P3 ^ 3;
+sbit MD_AR = P3 ^ 4;
 
 char cmd[20] = {0}; //chaine complete de commande
 char typeCmd[5] = {0}; //premières lettres de la commande; indiquant son type
+char currentPosCmd = 0; //iterateur sur la commande
 
 int distance; 
 
@@ -26,8 +29,8 @@ void init_1(){
 	WDTCN = 0xAD;
 	
 	XBR2 = XBR2 | 0x40;
-	P1MDOUT = P1MDOUT | 0x40;
-	P3MDOUT = P3MDOUT | 0x08;
+	//P1MDOUT = P1MDOUT | 0x40;
+	P3MDOUT = P3MDOUT | 0x1C;
 	
 	ADC0CN = ADC0CN | 0x80;
 	
@@ -42,55 +45,88 @@ void delay_10us(){
 	for(i=0;i<1;i++){}
 }
 
-void mesure_distance(typeCmd, cmd){
-	char* consigne;
-	char* currentPosConsigne;
+void trigger(){
 	MD=1;
-	if(typeCmd == "MD"){
-		while(cmd[currentPosCmd] != ' '){ //relecture MD
-			currentPosCmd++;
-		}
-		currentPosCmd++;
-		if(cmd[currentPosCmd] == 'Back'){ //'Back' pour l'arriere
-			MES_Dist_AR (void);
-		}
-		if(cmd[currentPosCmd] == 'Front'){ //'Front' pour l'arriere
-			MES_Dist_AV (void);
-		}
-	}
 	delay_10us();
 	MD=0;
 }
 
-unsigned int MES_Dist_AV (void){
+unsigned int MES_Dist_AV (void){ //Mesure distance avant
 	
-	while(cmd[currentPosCmd] != ' '){
-		currentPosCmd++;
-		consigne[currentPosCmd] = cmd[currentPosCmd];
-	}
-	delay_10us();
-	currentPosCmd = 0;
-}
-	
-unsigned int MES_Dist_AR (void){
-	
-	while(cmd[currentPosCmd] != ' '){
-		currentPosCmd++;
-		consigne[currentPosCmd] = cmd[currentPosCmd];
-	}
-	delay_10us();
-	currentPosCmd = 0;
-}
-
-void mesure() interrupt 18{
-	if((P3IF & 0x04)==0x04){
-		T4CON = T4CON | 0x04;
+	if((P3IF & 0x04)==0x04){ //P3IF port 3 interrupt flag
+		T4CON = T4CON | 0x04; //activation du timer4
 		P3IF = P3IF & 0xFB;
+		distance=0;
 	}
 	else{
-		distance = (TH4*256 + TL4)/12;
+		distance = (TH4*256 + TL4)/12; //conversion distance en cm
 		P3IF = P3IF | 0x04;
 	}
 	P3IF = P3IF & 0xBF;
+	return distance;
 }
+	
+unsigned int MES_Dist_AR (void){ //Mesure distance arriere 
+	
+	if((P3IF & 0x08)==0x08){
+		T4CON = T4CON | 0x04;
+		P3IF = P3IF & 0xFB;
+		distance=0;
+	}
+	else{
+		distance = (TH4*256 + TL4)/12;
+		P3IF = P3IF | 0x08;
+	}
+	P3IF = P3IF & 0xBF;
+	return distance;
+}
+
+void mesure_distance(typeCmd,char* cmd){
+	char* consigne;
+	char* currentPosConsigne;
+	if(typeCmd == "MD"){
+		while(cmd[currentPosCmd] != ' '){ //relecture MD
+			consigne[currentPosCmd] = cmd[currentPosCmd];
+			currentPosCmd++;
+		}
+	
+		if(cmd[currentPosCmd] == "Back"){ //'Back' pour l'arriere
+			MD_A=1;
+			MES_Dist_AR ();
+		}
+		if(cmd[currentPosCmd] == "Front"){ //'Front' pour l'arriere
+			MD_AR=1;
+			MES_Dist_AV ();
+		}	}
+
+	delay_10us();
+	currentPosCmd = 0;
+	MD_A=0;
+}
+
+//void main(){
+//	WDTCN = 0xDE;   // Devalidation du watchdog 
+//	WDTCN = 0xAD;
+
+//	OSCXCN =  0xef; //configure external oscillator
+//	delay_10us();
+//	while(!(OSCXCN&(0x80))){}
+
+//	OSCICN = OSCICN | 0x08; //utilise l'oscillateur externe
+//	
+//	init();
+//  //SBUF0 = 'T';
+//	while(1) 
+//	{
+//		// Gestion cmd
+//		if(cmdRecue){
+//			cmdRecue = 0;
+//			mesure_distance(typeCmd,cmd);
+//			
+//			
+//		}
+//	}
+//}
+
+
 
